3e1a36d332844eb64a6171490673e455a03923e1
Max Jala <maxjala@gmail.com>
2017-04-14 16:18:30 +0800

    Initial Commit

---
+++ Delaygram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Delaygram
+//
+//  Created by Max Jala on 14/04/2017.
+//  Copyright © 2017 TeamDiamonds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

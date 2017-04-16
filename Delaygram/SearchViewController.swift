693dc76f58b989312a112f21a73e7e593fa9b3ee
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-04-16 22:03:44 +0800

    just added search bar

---
+++ Delaygram/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Delaygram
+//
+//  Created by nicholaslee on 16/04/2017.
+//  Copyright © 2017 TeamDiamonds. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+}

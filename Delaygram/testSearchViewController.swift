b811219db8293b72eca4800fbb635e9569d2c8e6
nicholaslee <nicholaslee@nicholaslees-MacBook-Pro.local>
2017-04-17 18:48:09 +0800

    test searchviewcontroller has errors

--- Delaygram/testSearchViewController.swift
+++ Delaygram/testSearchViewController.swift
@@ -49,33 +49,40 @@ class testSearchViewController: UIViewController, UICollectionViewDataSource, UI
         listenToFirebase()
     }
     
-    func addUser(id: Any , postInfo:NSDictionary){
-        if let userID = postInfo["userID"] as? String,
-            let userScreenName = postInfo["userScreenName"] as? String,
-            let caption = postInfo["caption"] as? String,
-            let imageURL = postInfo["imageURL"] as? String,
-            let postID = id as? String,
+    func addPost(id: Any , postInfo:NSDictionary){
+        if let userEmail = postInfo["userEmail"] as? String,
+            let caption = postInfo["body"] as? String,
+            //let profilePictureURL = postInfo["profileImageURL"] as? String,
             let timeStamp = postInfo["timestamp"] as? String,
-            let currentPostID = Int(postID) {
-            let newPost = PicturePost(anID: currentPostID, aUserID: userID, aUserScreenName: userScreenName, aUserProfileImageURL: self.uploadImageURL, anImagePostURL: imageURL, aCaption: caption, aTimeStamp: timeStamp)
+            let postID = id as? String, //remember to do postID +=1
+            let currentPostID = Int(postID),
+            let userID =  postInfo["userID"] as? String,
+            let imagePostURL = postInfo["imageURL"] as? String {
+            
+            let newPost = PicturePost(anID: currentPostID, aUserID: userID, aUserScreenName: userEmail, aUserProfileImageURL: imagePostURL, anImagePostURL: imagePostURL, aCaption: caption, aTimeStamp: timeStamp)
             
             self.userPost.append(newPost)
         }
     }
     
+    
     func listenToFirebase() {
+        
         ref.child("posts").observe(.value, with: { (snapshot) in
             print("Value : " , snapshot)
         })
         
         // 2. get the snapshot
-        ref.child("users").observe(.childAdded, with: { (snapshot) in
+        ref.child("posts").observe(.childAdded, with: { (snapshot) in
             print("Value : " , snapshot)
             
             // 3. convert snapshot to dictionary
-            guard let info = snapshot.value as? NSDictionary else {return}
+            guard let info = snapshot.value as? NSDictionary else {
+            
+                return
+            }
             // 4. add student to array of messages
-            self.addUser(id: snapshot.key, postInfo: info)
+            self.addPost(id: snapshot.key, postInfo: info)
             
             // sort
             self.userPost.sort(by: { (post1, post2) -> Bool in
@@ -107,8 +114,8 @@ class testSearchViewController: UIViewController, UICollectionViewDataSource, UI
         
         let imageName = (indexPath.row % 2 == 0) ? "imageURL" : "imageURL"
         
-        let currentUser = userPost[indexPath.row]
-        let userImage = currentUser.imagePostURL
+        let currentPost = userPost[indexPath.row]
+        let userImage = currentPost.imagePostURL
         cell.imageView.image = UIImage(named: imageName)
         cell.imageView.loadImageUsingCacheWithUrlString(urlString: userImage)
         return cell

ad20653301ef761a06d68f6d4205a8d8552359b3
Sufiyan Shuib <p_ein@hotmail.com>
2017-04-18 09:27:00 +0800

    profile storyboard’s elements added (yet to be completed), codes, classes and all else still pending..

---
+++ Delaygram/ProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileViewController.swift
+//  Delaygram
+//
+//  Created by Stolen on 18/04/2017.
+//  Copyright © 2017 TeamDiamonds. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

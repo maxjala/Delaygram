74c2b8e3178864d9fe2d26bf01cded73704b06ee
Max Jala <maxjala@gmail.com>
2017-04-19 15:37:45 +0800

    added CommentsViewController set up generalProfileVC

---
+++ Delaygram/CommentsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CommentsViewController.swift
+//  Delaygram
+//
+//  Created by Max Jala on 19/04/2017.
+//  Copyright © 2017 TeamDiamonds. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
